b69ff606e388cf30e2f97a9adeab17350d5fe76c
miashin05 <mys2131@columbia.edu>
2026-02-17 18:34:53 -0500

    less clicky

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         for (const k of keys){
             const currentGain = activeOscillators[k].gainNode.gain;
             currentGain.cancelScheduledValues(currentTime);
-            currentGain.setTargetAtTime(targetGain, currentTime, 0.001);
+            currentGain.setTargetAtTime(targetGain, currentTime, 0.08);
 
         }
 
@@ -98,9 +98,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
             gainNode.gain.cancelScheduledValues(currentTime);    
             gainNode.gain.setValueAtTime(Math.max(gainNode.gain.value, 0.0001), currentTime);      
 
-            gainNode.gain.exponentialRampToValueAtTime(0.0001, currentTime + 0.02);
+            gainNode.gain.exponentialRampToValueAtTime(0.0001, currentTime + 0.08);
 
-            osc.stop(currentTime + 0.02);
+            osc.stop(currentTime + 0.08);
 
             delete activeOscillators[key];
             updateKeyGains();
